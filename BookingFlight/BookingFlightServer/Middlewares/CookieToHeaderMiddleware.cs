c715f726d5e208a8df646b4b8249c694a3af3607
= <96453867+vuvien07@users.noreply.github.com>
2025-06-27 13:54:18 +0700

    add cookie middleware

---
+++ BookingFlight/BookingFlightServer/Middlewares/CookieToHeaderMiddleware.cs
@@ -0,0 +1,23 @@
+﻿namespace BookingFlightServer.Middlewares
+{
+	public class CookieToHeaderMiddleware
+	{
+		private readonly RequestDelegate _next;
+
+		public CookieToHeaderMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task Invoke(HttpContext context)
+		{
+			if (context.Request.Cookies.TryGetValue("X-Access-Token", out var token))
+			{
+				context.Request.Headers["X-Access-Token"] = token;
+				context.Request.Headers["Authorization"] = $"Bearer {token}";
+			}
+
+			await _next(context);
+		}
+	}
+}
