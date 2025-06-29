b02becb1f51dc5b1542e17ab7123616024186c54
W-y-r-m <nguyenducthanh14523@gmail.com>
2025-06-29 18:13:53 +0700

    Add UI view list account (temp version)

---
+++ BookingFlight/BookingFlightClient/Controllers/ManageAccountController.cs
@@ -0,0 +1,21 @@
+﻿using BookingFlightClient.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingFlightClient.Controllers
+{
+    public class ManageAccountController : Controller
+    {
+        private readonly HttpClient httpClient;
+
+        public ManageAccountController(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var apiUrl = "http://localhost:5077/api/ManageAccount/get-all-account";
+            var accountDTOs = await httpClient.GetFromJsonAsync<List<ResponseAccountDTO>>(apiUrl);
+            return View(accountDTOs);
+        }
+    }
+}
---
+++ BookingFlight/BookingFlightClient/Models/DTO/ResponseAccountDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BookingFlightClient.Models.DTO
+{
+    public class ResponseAccountDTO
+    {
+        public int AccountId { get; set; }
+        public string Username { get; set; }
+        //public string Password { get; set; }
+        public string Role { get; set; }
+        public string Status { get; set; }
+    }
+
+}
