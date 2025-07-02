4a35075e9fdcdef1c719852e8ce06365788e1749
= <96453867+vuvien07@users.noreply.github.com>
2025-07-02 10:11:15 +0700

    add mapper

---
+++ BookingFlight/BookingFlightServer/Mappers/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BookingFlightServer.DTO.Shared;
+using BookingFlightServer.Entities;
+
+namespace BookingFlightServer.Mappers
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<Ticket, TicketDTO>().
+				ForMember(c => c.CustomerDTO, opt => opt.MapFrom(c => c.Customer)).
+				ForMember(c => c.FlightDTO, opt => opt.MapFrom(c => c.Flight)).
+				ForMember(c => c.ClassSeatDTO, opt => opt.MapFrom(c => c.ClassSeat)).
+				ReverseMap();
+		}
+	}
+}
