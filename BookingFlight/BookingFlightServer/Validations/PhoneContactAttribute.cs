ef9f34a8cc8cac962457550e351b39e687f6ceb7
= <96453867+vuvien07@users.noreply.github.com>
2025-07-02 10:09:15 +0700

    add some validation with ignore paths

---
+++ BookingFlight/BookingFlightServer/Validations/PhoneContactAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace BookingFlightServer.Validations
+{
+	public class PhoneContactAttribute:ValidationAttribute
+	{
+		private readonly string[] _ignorePaths;
+
+		public PhoneContactAttribute(params string[] ignorePaths)
+		{
+			_ignorePaths = ignorePaths;
+		}
+
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			var httpContextAccessor = validationContext.GetService(typeof(IHttpContextAccessor)) as IHttpContextAccessor;
+
+			var requestPath = httpContextAccessor?.HttpContext?.Request?.Path.Value ?? string.Empty;
+
+			if (_ignorePaths.Any(p => requestPath.Contains(p)))
+			{
+				return ValidationResult.Success;
+			}
+
+			if (value is not string phone)
+			{
+				return new ValidationResult("Dữ liệu không hợp lệ.");
+			}
+
+			if (string.IsNullOrEmpty(phone))
+			{
+				return new ValidationResult("Số liên hệ không được để trống");
+			}
+			if (!Regex.IsMatch(phone, @"^(0|\+84)(3|5|7|8|9)+([0-9]{8})$"))
+			{
+				return new ValidationResult("Số liên hệ không đúng định dạng");
+			}
+			return ValidationResult.Success;
+		}
+	}
+}
