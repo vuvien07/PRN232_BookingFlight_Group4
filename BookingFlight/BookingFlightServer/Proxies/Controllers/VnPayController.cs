7efaa52deca577cd83abfb325f90459e17e49ca7
= <96453867+vuvien07@users.noreply.github.com>
2025-07-02 10:11:00 +0700

    modify proxies

--- BookingFlight/BookingFlightServer/Proxies/Controllers/VnPayController.cs
+++ BookingFlight/BookingFlightServer/Proxies/Controllers/VnPayController.cs
@@ -5,23 +5,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookingFlightServer.Proxies.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class VnPayController : ControllerBase
-    {
-        private readonly IVnPayService _vnPayService;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class VnPayController : ControllerBase
+	{
+		private readonly IVnPayService _vnPayService;
 
 		public VnPayController(IVnPayService vnPayService)
-        {
+		{
 			_vnPayService = vnPayService;
-        }
+		}
 
-        [HttpPost("create-payment-url")]
-        public IActionResult CreatePaymentUrl([FromBody] VnPayTransactionInformationDTO transactionInformation)
-        {
+		[HttpPost("create-payment-url")]
+		public IActionResult CreatePaymentUrl([FromBody] VnPayTransactionInformationDTO transactionInformation)
+		{
 			var url = _vnPayService.createPaymentUrl(transactionInformation, this.HttpContext);
 			return Ok(new { url = url });
-        }
+		}
 
-    }
+		[HttpGet("payment-execute")]
+		public IActionResult PaymentExecute()
+		{
+			var collection = Request.Query;
+			var response = _vnPayService.paymentExecute(collection);
+			if (response == null)
+			{
+				return BadRequest(new { message = "Payment execution failed" });
+			}
+			return Ok(response);
+		}
+	}
 }
