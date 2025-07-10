7476860570d238bf26556eec61b72bb55a084da4
W-y-r-m <nguyenducthanh14523@gmail.com>
2025-07-10 22:30:44 +0700

    Add role repository

---
+++ BookingFlight/BookingFlightServer/Repositories/IRoleRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookingFlightServer.Entities;
+
+namespace BookingFlightServer.Repositories
+{
+    public interface IRoleRepository
+    {
+        Task<List<Role>?> GetRolesAsync();
+    }
+}
