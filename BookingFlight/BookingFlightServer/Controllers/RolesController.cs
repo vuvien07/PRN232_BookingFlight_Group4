e14e5649d6420bff65084202925447f66ece943d
W-y-r-m <nguyenducthanh14523@gmail.com>
2025-07-10 22:32:00 +0700

    Add RoleController

---
+++ BookingFlight/BookingFlightServer/Controllers/RolesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingFlightServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        public async Task<IActionResult> GetRoles()
+        {
+            // get
+
+            return Ok();
+        }
+    }
+}
