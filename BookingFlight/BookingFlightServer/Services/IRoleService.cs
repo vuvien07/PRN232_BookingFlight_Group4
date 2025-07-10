a882dba88dc8bdeedc8e079cdb84f23d616b6e5e
W-y-r-m <nguyenducthanh14523@gmail.com>
2025-07-10 22:30:24 +0700

    Add role service

---
+++ BookingFlight/BookingFlightServer/Services/IRoleService.cs
@@ -0,0 +1,9 @@
+﻿using BookingFlightServer.DTO.Shared;
+
+namespace BookingFlightServer.Services
+{
+    public interface IRoleService
+    {
+        Task<List<RoleDTO>?> GetRoleDTOsAsync();
+    }
+}
