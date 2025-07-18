8f1f274efa66281565b84595354e654ef26948ff
= <96453867+vuvien07@users.noreply.github.com>
2025-07-18 09:21:14 +0700

    update server controllers

---
+++ BookingFlight/BookingFlightServer/Controllers/TicketController.cs
@@ -0,0 +1,24 @@
+﻿using BookingFlightServer.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingFlightServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly ITicketService _ticketService;
+
+		public TicketController(ITicketService ticketService)
+        {
+            _ticketService = ticketService;
+        }
+        [Route("getByTicketNumber")]
+		[HttpGet]
+        public async Task<IActionResult> GetByTicketNumber([FromQuery] string? ticketNumber)
+        {
+            return Ok(await _ticketService.GetByTicketNumber(ticketNumber));
+        }
+    }
+}
