93b6d6dcfad6726fbacc472ce8fc8b340a57e677
= <96453867+vuvien07@users.noreply.github.com>
2025-07-18 09:18:44 +0700

    add ai intergration

---
+++ BookingFlight/BookingFlightServer/Proxies/Services/IGeminiService.cs
@@ -0,0 +1,14 @@
+﻿using BookingFlightServer.DTO.Filter;
+using BookingFlightServer.DTO.Query;
+using BookingFlightServer.Proxies.DTO;
+using BookingFlightServer.Utils;
+
+namespace BookingFlightServer.Proxies.Services
+{
+	public interface IGeminiService
+	{
+		Task<string> AskGeminiAsync(GeminiConversationDTO geminiConversationDT, HttpContext httpContext, FlightSearchSessionStore flightSearchSessionStore);
+		Task<List<FlightQueryDTO>> GetFlightsWithGemini(FilterFlightDTO filterFlightDTO);
+		Task<List<dynamic>> GetAllFlightSeatWithGeminiByFlightId(int flightId);
+	}
+}
