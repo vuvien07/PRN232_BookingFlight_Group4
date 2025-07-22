a443f5712306eb1eba6ec30823362df4f237d4c4
= <96453867+vuvien07@users.noreply.github.com>
2025-07-22 14:26:04 +0700

    add and update client models

--- BookingFlight/BookingFlightClient/Models/DTO/ResponseTicketDTO.cs
+++ BookingFlight/BookingFlightClient/Models/DTO/ResponseTicketDTO.cs
@@ -20,7 +20,6 @@ namespace BookingFlightClient.Models.DTO
 		public DateOnly DateOfBirth { get; set; }
 
 		public string FullName { get; set; } = null!;
-
 		public ClassSeatDTO ClassSeatDTO { get; set; } = null!;
 
 		public string? ContactFullName { get; set; }
@@ -31,6 +30,7 @@ namespace BookingFlightClient.Models.DTO
 
 		public string? ContactAddress { get; set; }
 		public FlightDTO FlightDTO { get; set; } = null!;
+		public SeatDTO Seat { get; set; } = null!;
 		public List<TicketItemDTO> TicketItems { get; set; } = new List<TicketItemDTO>();
 	}
 }
